b142341cb0bc606196f3ee6fcddbe631d79dcd5b
unknown <Chandy.Neat@PNC.LAN>
2024-10-29 16:28:58 +0700

    push code to branch

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const navbar = document.querySelector('.navbar ');
-    if (navbar.style.display === 'flex') {
-        navbar.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
-}
